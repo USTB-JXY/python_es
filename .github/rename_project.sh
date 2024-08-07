7ec2539f48402629abb10fa39019ee258eae0ada
USTB-JXY <USTB-JXY@users.noreply.github.com>
2024-08-07 10:20:45 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="USTB-JXY"
+original_name="python_es"
+original_urlname="python_es"
+original_description="Awesome python_es created by USTB-JXY"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_es $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ python_es/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python_es."""
+
+from python_es.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
